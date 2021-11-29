d06a859be31879d6f3c307e8274de46106178568
EstrellaDionis <EstrellaDionis@gmail.com>
2021-11-29 16:26:13 -0500

    created core dom for project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
+  
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todos</h1>
+        <ul>
+          <li>Workout</li>
+          <li>Eat Breakfast</li>
+          <li>Daily Warmups</li>
+        </ul>
+
+        <form>
+          <input />
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     );
   }
