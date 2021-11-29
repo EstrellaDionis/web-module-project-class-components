d8da49e72cd1f70a91bbfba9e259765b5eb503c7
EstrellaDionis <EstrellaDionis@gmail.com>
2021-11-29 16:38:29 -0500

    built state and added functionality to see if something was completed by giving a notification of completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 
 class App extends React.Component {
-  
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+          {
+            task: 'Organize Garage',
+            id: 1528817077286,
+            completed: false
+          },
+          {
+            task: 'Bake Cookies',
+            id: 1528817084358,
+            completed: false
+          }
+      ]
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
+    const { todos } = this.state;
+    console.log(todos);
     return (
       <div>
         <h1>Todos</h1>
         <ul>
-          <li>Workout</li>
-          <li>Eat Breakfast</li>
-          <li>Daily Warmups</li>
+          {
+            todos.map(todo => {
+              return (<li>{todo.task} {todo.completed? <span>- completed</span> : <span></span>}</li>)
+            })
+          }
+         
         </ul>
 
         <form>
